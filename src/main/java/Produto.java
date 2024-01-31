afb431d9f7613332b6157e778b12c09dd805ec97
Rafael Duarte <rafael@prismarede.com.br>
2024-01-31 15:50:46 -0300

    ClasseProduto

---
+++ src/main/java/Produto.java
@@ -0,0 +1,18 @@
+public class Produto {
+
+
+    // Cria a classe Produto com seus respectivos atributos:
+
+    // Nome (Texto)
+    // Descrição (Texto)
+    // Preço Unitário (ponto flutuante)
+    // Quantidade (numero).
+
+
+    private String nome;
+    private String descricao;
+    private double precoUnitario;
+    private int quantidade;
+
+
+}
